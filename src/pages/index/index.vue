5c2e3e81b6990f1aa4b3981c029b7589258ef30c
Gitea <gitea@fake.local>
2024-08-07 09:54:54 +0800

    猜你喜欢模块导入引用，数据获取和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
-import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
 import type { BannerItem, CategoryItem } from '@/types/home'
 import { ref } from 'vue'
 import CategoryPanel from './components/CategoryPanel.vue'
+import HotPanel from './components/HotPanel.vue'
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
 
@@ -22,24 +23,43 @@ const getHomeCategoryData = async () => {
   console.log(res.result)
 }
 
+// 获取热门推荐数据
+const hotList = ref<HotItem[]>([])
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
 onLoad(() => {
   getHomeBannerData()
   getHomeCategoryData()
+  getHomeHotData()
 })
 </script>
 
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <!--自定义轮播图 -->
-  <XtxSwiper :list="bannerList" />
-  <!-- 自定义导航栏 -->
-  <CategoryPanel :list="categoryList" />
-  <view class="index">index</view>
+  <scroll-view class="scroll-view" scroll-y>
+    <!--自定义轮播图 -->
+    <XtxSwiper :list="bannerList" />
+    <!-- 自定义导航栏 -->
+    <CategoryPanel :list="categoryList" />
+    <!-- 热门推荐模块 -->
+    <HotPanel :list="hotList" />
+    <!-- 猜你喜欢模块 -->
+    <XtxGuess />
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
 }
 </style>
--- src/services/home.ts
+++ src/services/home.ts
@@ -35,3 +35,25 @@ export const getHomeCategoryAPI = () => {
     url: '/home/category/mutli',
   })
 }
+
+// services/home.ts
+/**
+ * 首页-热门推荐-小程序
+ */
+export const getHomeHotAPI = () => {
+  return http<HotItem[]>({
+    method: 'GET',
+    url: '/home/hot/mutli',
+  })
+}
+
+/**
+ * 猜你喜欢-小程序
+ */
+export const getHomeGoodsGuessLikeAPI = (data?: PageParams) => {
+  return http<PageResult<GuessItem>>({
+    method: 'GET',
+    url: '/home/goods/guessLike',
+    data,
+  })
+}
